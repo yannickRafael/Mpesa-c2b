2edf2b7df78c70683bae930e6676ad2d735dd01f
Helder Mabasso <hfmabasso@gmail.com>
2024-11-28 21:28:57 +0200

    Importing API keys and information from .env file

--- mpesa.js
+++ mpesa.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const Mpesa = require('./mpesa-lib.js'); // Path to the mpesa.js file you created
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(
@@ -14,13 +17,13 @@ app.use(
 
 // Your M-Pesa API credentials and configuration
 const mpesaConfig = {
-    public_key: 'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAmptSWqV7cGUUJJhUBxsMLonux24u+FoTlrb+4Kgc6092JIszmI1QUoMohaDDXSVueXx6IXwYGsjjWY32HGXj1iQhkALXfObJ4DqXn5h6E8y5/xQYNAyd5bpN5Z8r892B6toGzZQVB7qtebH4apDjmvTi5FGZVjVYxalyyQkj4uQbbRQjgCkubSi45Xl4CGtLqZztsKssWz3mcKncgTnq3DHGYYEYiKq0xIj100LGbnvNz20Sgqmw/cH+Bua4GJsWYLEqf/h/yiMgiBbxFxsnwZl0im5vXDlwKPw+QnO2fscDhxZFAwV06bgG0oEoWm9FnjMsfvwm0rUNYFlZ+TOtCEhmhtFp+Tsx9jPCuOd5h2emGdSKD8A6jtwhNa7oQ8RtLEEqwAn44orENa1ibOkxMiiiFpmmJkwgZPOG/zMCjXIrrhDWTDUOZaPx/lEQoInJoE2i43VN/HTGCCw8dKQAwg0jsEXau5ixD0GUothqvuX3B9taoeoFAIvUPEq35YulprMM7ThdKodSHvhnwKG82dCsodRwY428kg2xM/UjiTENog4B6zzZfPhMxFlOSFX4MnrqkAS+8Jamhy1GgoHkEMrsT5+/ofjCx0HjKbT5NuA2V/lmzgJLl3jIERadLzuTYnKGWxVJcGLkWXlEPYLbiaKzbJb2sYxt+Kt5OxQqC1MCAwEAAQ==',
-    api_host: 'api.sandbox.vm.co.mz',
-    api_key: 'j7jqp5stcivruxoejh7oo18h124wbe9t',
-    origin: 'developer.mpesa.vm.co.mz',
-    service_provider_code: '171717',
-    initiator_identifier: 'FenixBot',
-    security_credential: '<Security Credential>'
+    public_key: process.env.PUBLIC_KEY,
+    api_host: process.env.API_HOST,
+    api_key: process.env.API_KEY,
+    origin: process.env.ORIGIN,
+    service_provider_code: process.env.SERVICE_PROVIDER,
+    initiator_identifier: process.env.INITIATOR_IDENTIFIER,
+    security_credential: process.env.SECURITY_CREDENTIAL
 };
 
 // Initialize the Mpesa object with your configuration
